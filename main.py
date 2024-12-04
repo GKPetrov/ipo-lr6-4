2f8f87c3ac12e3556c8539eb5577d65e06439c01
Gleb <glebpetrof2007@gmail.com>
2024-12-04 16:28:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-Prompt=str(input('Введите строку для поиска'))
-a=list()
+Prompt = input('Введите строку для поиска')
+a = []
 with open("text.txt", "r") as file:
     for line in file:
         if Prompt in line:
             print(line)
             a.extend(line)
-    b=''.join(a)
-    list=b.split('\n')
+    b = ''.join(a)
+    list = b.split('\n')
     list.sort()
     print(list)
